d504806ed70702366c8299ca5a2cc656a9eea088
LeonardoRios18 <LeonardoRios_Beta18@users.noreply.github.com>
2019-05-16 18:09:32 -0500

    Initial commit

---
+++ Ecuaciones_CEcuacion.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N4_Pag_74
+{
+    class Ecuacion_CEcuacion
+    {
+        //El termino de mayor grado tiene exponenete 3 fijo
+        private double c3, c2, c1, c0; // coeficientes
+
+        public CEcuacion(double a, double b, double c, double d)
+        {
+            c3 = a; c2 = b; c1 = c; c0 = d;
+        }
+
+        public double ValorPara(double x)
+        {
+            double resultado;
+            resultado = c3 * x * x * x + c2 * x * x + c1 * x + c0;
+            return resultado;// devolver el valor calculado
+        }
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
---
+++ Programa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_N4_Pag_74
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el valor A:");
+
+            double c1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor B:");
+            double c2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor C:");
+            double c3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor D:");
+            double c4 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el valor X:");
+            double x= double.Parse(Console.ReadLine());
+
+            Ecuacion_CEcuacion clase = new Ecuacion_CEcuacion(c1, c2, c3, c4);
+            Console.WriteLine(clase.ValorPara(x));
+
+            Console.ReadKey();
+          }
+    }
+}
